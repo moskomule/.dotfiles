5fa2094e327216185b0e0824dd4489e074afb729
moskomule <hataya@keio.jp>
2017-12-08 13:40:26 +0900

    set nvim's path here

--- setup.sh
+++ setup.sh
@@ -9,17 +9,3 @@ for file in .??*; do
     fi
     ln -fsv $(pwd)/$file $HOME/$file
 done
-
-CONFIG=$HOME"/.config"
-DEIN=$CONFIG"/dein"
-if [[ ! -e CONFIG ]]; then
-    mkdir $CONFIG
-fi
-if [[ ! -e $DEIN ]]; then
-    mkdir $DEIN
-fi
-
-# nvim setup
-ln -sv .vim $CONFIG/nvim
-ln -sv .vimrc $CONFIG/nvim/init.vim
-ln -sv dein/plugins.toml $DEIN/plugins.toml
