054059f135fe75f5bd95221e5d5fffe1e488bb34
moskomule <hataya@keio.jp>
2019-03-04 15:34:05 +0900

    update

--- getconda.sh
+++ getconda.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+if [ "$(uname)" == 'Darwin' ]; then
+    URL=https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh 
+else
+    URL=https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh 
+fi
+wget $URL -O miniconda.sh
 bash miniconda.sh -b -p $HOME/.miniconda
 touch $HOME/.zshrc_
 echo export PATH="$HOME/.miniconda/bin:$PATH" >> $HOME/.zshrc_
