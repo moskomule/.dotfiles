e93a08298cde4e2fb506d2bf8eb6e13d9c589f0a
moskomule <hataya@keio.jp>
2019-07-06 16:11:34 +0900

    for simplicity

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+DOTFILE_DIR=${HOME}/.dotfiles
+
 cd ${HOME}
 git clone --recursive https://github.com/moskomule/.dotfiles.git
-cd .dotfiles
+cd ${DOTFILE_DIR}
 
 if [[ -e ${HOME}/.zshrc ]]; then
     mv ${HOME}/.zshrc ${HOME}/.zshrc.backup
@@ -12,19 +14,19 @@ if [[ -e ${HOME}/.vimrc ]]; then
     mv ${HOME}/.vimrc ${HOME}/.vimrc.backup
 fi
 
-echo "source ${PWD}/zsh/shared" >> ${HOME}/.zshrc
+echo "source ${DOTFILE_DIR}/zsh/shared" >> ${HOME}/.zshrc
 
 if [[ ${XDG_CONFIG_HOME} == "" ]]; then
     XDG_CONFIG_HOME=${HOME}/.config
     echo "export XDG_CONFIG_HOME=${HOME}/.config" >> ${HOME}/.zshrc
 fi
 
-ln -s ${PWD}/.tmux.conf ${HOME}/.tmux.conf
-ln -s ${PWD}/nvim/init.vim ${HOME}/.vimrc
+ln -s ${DOTFILE_DIR}/.tmux.conf ${HOME}/.tmux.conf
+ln -s ${DOTFILE_DIR}/nvim/init.vim ${HOME}/.vimrc
 mkdir -p ${XDG_CONFIG_HOME}/nvim
-ln -s ${PWD}/nvim/init.vim ${XDG_CONFIG_HOME}/nvim/init.vim
+ln -s ${DOTFILE_DIR}/nvim/init.vim ${XDG_CONFIG_HOME}/nvim/init.vim
 
-if [[ -e "${PWD}/zsh/pure" ]]; then
-    ln -s ${PWD}/zsh/pure/pure.zsh ${PWD}/zsh/prompt_pure_setup
-    ln -s ${PWD}/zsh/pure/async.zsh ${PWD}/zsh/async
+if [[ -e "${DOTFILE_DIR}/zsh/pure" ]]; then
+    ln -s ${DOTFILE_DIR}/zsh/pure/pure.zsh ${DOTFILE_DIR}/zsh/prompt_pure_setup
+    ln -s ${DOTFILE_DIR}/zsh/pure/async.zsh ${DOTFILE_DIR}/zsh/async
 fi
